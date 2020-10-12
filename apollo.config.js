08f6399f999957dfbe7cd52e2f7bc61396dc143a
tblattel <72397330+tblattel@users.noreply.github.com>
2020-10-12 16:36:37 -0400

    Initial commit

---
+++ apollo.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+// Load .env files
+const { loadEnv } = require('vue-cli-plugin-apollo/utils/load-env')
+const env = loadEnv([
+  path.resolve(__dirname, '.env'),
+  path.resolve(__dirname, '.env.local')
+])
+
+module.exports = {
+  
+}
